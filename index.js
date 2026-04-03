772c99c1b35ecf3bdb100fcf7cd2a751685395c6
Pankaj <pankajpriscilla@gmail.com>
2026-04-03 02:19:00 -0400

    Refactored
    Signed-off-by: Pankaj <pankajpriscilla@gmail.com>

--- index.js
+++ index.js
@@ -1,8 +1,29 @@
-const jwtLib = require( './lib/jwtLib' );
-const jwtRoles = require( './lib/jwtRoles' );
+const jwtLib = require('./lib/jwtLib');
+const jwtRoles = require('./lib/jwtRoles');
 
 module.exports = {
-    // From pkg-jwt-read
+    jwtVerify: jwtLib.verifyJwt,
+    jwtVerifyWebToken: jwtLib.verifyWebToken,
+    jwtVerifyNoThrow: jwtLib.verifyJwtNoThrow,
+    jwtVerifyWebTokenNoThrow: jwtLib.verifyWebTokenNoThrow,
+    jwtVerifyVisitorNoThrow: jwtLib.verifyVisitorNoThrow,
+    jwtGetClientId: jwtLib.jwtClientId,
+    jwtGetVisitorClientId: jwtLib.visitorClientId,
+    jwtIsExpired: jwtLib.isJwtExpired,
+    jwtGetAgeInSeconds: jwtLib.jwtAgeInSeconds,
+    jwtVerifyAndHasRole: jwtLib.verifyJwtAndRole,
+    jwtGetRoleName: jwtRoles.getNameOfRoleFromCode,
+    jwtGetRoleCode: jwtRoles.getCodeFromNameOfRole,
+    jwtValidateAndExtract: jwtLib.validateAndExtractJwtObject,
+    jwtValidateAndExtractWebToken: jwtLib.validateAndExtractWebToken,
+    jwtValidateAndExtractNoThrow: jwtLib.validateAndExtractJwtObjectNoThrow,
+    jwtValidateAndExtractWebTokenNoThrow: jwtLib.validateAndExtractWebTokenObjectNoThrow,
+    jwtValidateAndExtractVisitorNoThrow: jwtLib.validateAndExtractVisitorObjectNoThrow,
+
+
+    /**
+     * @Deprecated use new names starting with jwt...
+     */
     verifyJwt: jwtLib.verifyJwt,
     verifyWebToken: jwtLib.verifyWebToken,
     verifyJwtNoThrow: jwtLib.verifyJwtNoThrow,
