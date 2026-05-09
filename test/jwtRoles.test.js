5f14cce0ac02c800cb453bc0690d2e529e689f17
Singh Pankaj K <8635636+singh-pankaj-k@users.noreply.github.com>
2026-05-09 01:32:37 -0400

    Fix/coverage (#6)
    * Coverage added.
    
    Signed-off-by: Pankaj <pankajpriscilla@gmail.com>
    
    * Coverage added.
    
    Signed-off-by: Pankaj <pankajpriscilla@gmail.com>
    
    ---------
    
    Signed-off-by: Pankaj <pankajpriscilla@gmail.com>

--- test/jwtRoles.test.js
+++ test/jwtRoles.test.js
@@ -1,36 +1,35 @@
-const { describe, it } = require( "mocha" );
-const assert = require( "assert" );
-const jwtRoles = require( '../lib/jwtRoles' );
+const { describe, it } = require('mocha');
+const assert = require('assert');
+const jwtRoles = require('../lib/jwtRoles');
 
-describe( "Lib jwt Roles", function () {
+describe('Lib jwt Roles', function () {
+  describe('getNameOfRoleFromCode', function () {
+    it('should return admin for ad', function () {
+      assert.strictEqual(jwtRoles.getNameOfRoleFromCode('ad'), 'admin');
+    });
 
-    describe( "getNameOfRoleFromCode", function () {
-        it( "should return admin for ad", function () {
-            assert.strictEqual( jwtRoles.getNameOfRoleFromCode( "ad" ), "admin" );
-        } );
+    it('should return super_admin for su', function () {
+      assert.strictEqual(jwtRoles.getNameOfRoleFromCode('su'), 'super_admin');
+    });
 
-        it( "should return super_admin for su", function () {
-            assert.strictEqual( jwtRoles.getNameOfRoleFromCode( "su" ), "super_admin" );
-        } );
+    it('should return empty string for unknown code', function () {
+      assert.strictEqual(jwtRoles.getNameOfRoleFromCode(), '');
+      assert.strictEqual(jwtRoles.getNameOfRoleFromCode('unknown'), '');
+    });
+  });
 
-        it( "should return empty string for unknown code", function () {
-            assert.strictEqual( jwtRoles.getNameOfRoleFromCode(), "" );
-            assert.strictEqual( jwtRoles.getNameOfRoleFromCode( "unknown" ), "" );
-        } );
-    } );
+  describe('getCodeFromNameOfRole', function () {
+    it('should return ad for admin', function () {
+      assert.strictEqual(jwtRoles.getCodeFromNameOfRole('admin'), 'ad');
+    });
 
-    describe( "getCodeFromNameOfRole", function () {
-        it( "should return ad for admin", function () {
-            assert.strictEqual( jwtRoles.getCodeFromNameOfRole( "admin" ), "ad" );
-        } );
+    it('should return su for super_admin', function () {
+      assert.strictEqual(jwtRoles.getCodeFromNameOfRole('super_admin'), 'su');
+    });
 
-        it( "should return su for super_admin", function () {
-            assert.strictEqual( jwtRoles.getCodeFromNameOfRole( "super_admin" ), "su" );
-        } );
-
-        it( "should return empty string for unknown name", function () {
-            assert.strictEqual( jwtRoles.getCodeFromNameOfRole(), "" );
-            assert.strictEqual( jwtRoles.getCodeFromNameOfRole( "unknown" ), "" );
-        } );
-    } );
-} );
+    it('should return empty string for unknown name', function () {
+      assert.strictEqual(jwtRoles.getCodeFromNameOfRole(), '');
+      assert.strictEqual(jwtRoles.getCodeFromNameOfRole('unknown'), '');
+    });
+  });
+});
--- test/keys/keys.js
+++ test/keys/keys.js
@@ -1,12 +1,8 @@
 const privateKey =
-  '-----BEGIN PRIVATE KEY-----\n' +
-  'MC4CAQAwBQYDK2VwBCIEIFDSgS6vALkswz0iiAfyYDh+QJnJa+OXSKfMAk/nkA6L\n' +
-  '-----END PRIVATE KEY-----';
+  '-----BEGIN PRIVATE KEY-----\n' + 'MC4CAQAwBQYDK2VwBCIEIFDSgS6vALkswz0iiAfyYDh+QJnJa+OXSKfMAk/nkA6L\n' + '-----END PRIVATE KEY-----';
 
 const publicKey =
-  '-----BEGIN PUBLIC KEY-----\n' +
-  'MCowBQYDK2VwAyEAaXXgySpK3+derfui5rZR+VKDbU0goznsdu65nsHo2ls=\n' +
-  '-----END PUBLIC KEY-----';
+  '-----BEGIN PUBLIC KEY-----\n' + 'MCowBQYDK2VwAyEAaXXgySpK3+derfui5rZR+VKDbU0goznsdu65nsHo2ls=\n' + '-----END PUBLIC KEY-----';
 
 const hashKey = '240gTxVT2KnXOP4W6OdFkSEsdDWLqhLO2OP68o';
 
