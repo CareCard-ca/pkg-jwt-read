b2e3edd450b612b7869bf7e2e7e7190af0201c6e
Singh Pankaj K <8635636+singh-pankaj-k@users.noreply.github.com>
2026-05-09 01:32:37 -0400

    Fix/coverage (#6)
    * Coverage added.
    
    Signed-off-by: Pankaj <pankajpriscilla@gmail.com>
    
    * Coverage added.
    
    Signed-off-by: Pankaj <pankajpriscilla@gmail.com>
    
    ---------
    
    Signed-off-by: Pankaj <pankajpriscilla@gmail.com>

--- test/jwtRoles.test.js
+++ test/jwtRoles.test.js
@@ -8,33 +8,28 @@ describe('Lib jwt Roles', function () {
       assert.strictEqual(jwtRoles.getNameOfRoleFromCode('ad'), 'admin');
     });
 
-    describe( "getNameOfRoleFromCode", function () {
-        it( "should return admin for ad", function () {
-            assert.strictEqual( jwtRoles.getNameOfRoleFromCode( "ad" ), "admin" );
-        } );
+    it('should return super_admin for su', function () {
+      assert.strictEqual(jwtRoles.getNameOfRoleFromCode('su'), 'super_admin');
+    });
 
-        it( "should return super_admin for su", function () {
-            assert.strictEqual( jwtRoles.getNameOfRoleFromCode( "su" ), "super_admin" );
-        } );
+    it('should return empty string for unknown code', function () {
+      assert.strictEqual(jwtRoles.getNameOfRoleFromCode(), '');
+      assert.strictEqual(jwtRoles.getNameOfRoleFromCode('unknown'), '');
+    });
+  });
 
-        it( "should return empty string for unknown code", function () {
-            assert.strictEqual( jwtRoles.getNameOfRoleFromCode(), "" );
-            assert.strictEqual( jwtRoles.getNameOfRoleFromCode( "unknown" ), "" );
-        } );
-    } );
+  describe('getCodeFromNameOfRole', function () {
+    it('should return ad for admin', function () {
+      assert.strictEqual(jwtRoles.getCodeFromNameOfRole('admin'), 'ad');
+    });
 
-    describe( "getCodeFromNameOfRole", function () {
-        it( "should return ad for admin", function () {
-            assert.strictEqual( jwtRoles.getCodeFromNameOfRole( "admin" ), "ad" );
-        } );
+    it('should return su for super_admin', function () {
+      assert.strictEqual(jwtRoles.getCodeFromNameOfRole('super_admin'), 'su');
+    });
 
-        it( "should return su for super_admin", function () {
-            assert.strictEqual( jwtRoles.getCodeFromNameOfRole( "super_admin" ), "su" );
-        } );
-
-        it( "should return empty string for unknown name", function () {
-            assert.strictEqual( jwtRoles.getCodeFromNameOfRole(), "" );
-            assert.strictEqual( jwtRoles.getCodeFromNameOfRole( "unknown" ), "" );
-        } );
-    } );
-} );
+    it('should return empty string for unknown name', function () {
+      assert.strictEqual(jwtRoles.getCodeFromNameOfRole(), '');
+      assert.strictEqual(jwtRoles.getCodeFromNameOfRole('unknown'), '');
+    });
+  });
+});
